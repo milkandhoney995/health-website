fe07efbb3ab50cd213d1344e649a0603910b26d4
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-02 08:51:05 +0900

    headerのcssを追加

--- components/Header.vue
+++ components/Header.vue
@@ -3,12 +3,22 @@
     <div class="header__pc__fix">
       <div class="center">
         <nav class="menu">
-          <ul class="menu__pc menu__alignment">
-            <li>ABOUT</li>
-            <li>SERVICE</li>
-            <li>BLOG</li>
-            <li>RECIPE</li>
-            <li>CONTACT</li>
+          <ul class="menu__pc menu__alignment flex">
+            <li>
+              <a href="" class="link__fade menu__item">ABOUT</a>
+            </li>
+            <li>
+              <a href="" class="link__fade menu__item">SERVICE</a>
+            </li>
+            <li>
+              <a href="" class="link__fade menu__item">BLOG</a>
+            </li>
+            <li>
+              <a href="" class="link__fade menu__item">RECIPE</a>
+            </li>
+            <li>
+              <a href="" class="link__fade menu__item">CONTACT</a>
+            </li>
           </ul>
           <ul class="sns__pc menu__alignment">
             <li>facebook</li>
