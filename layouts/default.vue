834af4ca17e3947be8bc8a2711288b2a18782bb4
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-03 13:54:23 +0900

    Overlayをlayoutsに追加

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,16 @@
   <div>
     <Header />
     <Nuxt />
+    <Overlay />
   </div>
 </template>
 <script>
 import Header from '@/components/Header'
+import Overlay from '../components/Overlay.vue'
 export default {
   components: {
     Header,
+    Overlay,
   },
 }
 </script>
