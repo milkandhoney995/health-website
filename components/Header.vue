2c58ba9f4582eade9c83e313f345df9e37f665c9
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-03 11:26:01 +0900

    spanの:classを修正

--- components/Header.vue
+++ components/Header.vue
@@ -33,28 +33,28 @@
       <div class="center">
         <div class="logo">
           <a>
-            <h1>UTANO HARADA</h1>
+            <h1 class="logo__title">UTANO HARADA</h1>
           </a>
         </div>
         <div class="btn">
           <div class="hamburger__btn" @click="ActiveBtn = !ActiveBtn">
             <span
               class="hamburger__item"
-              :class="{ btn_line01: ActiveBtn }"
+              :class="{ item__change01: ActiveBtn }"
             ></span>
             <span
               class="hamburger__item"
-              :class="{ btn_line02: ActiveBtn }"
+              :class="{ item__change02: ActiveBtn }"
             ></span>
             <span
               class="hamburger__item"
-              :class="{ btn_line03: ActiveBtn }"
+              :class="{ item__change03: ActiveBtn }"
             ></span>
           </div>
         </div>
 
         <transition name="hamburger__menu">
-          <div class="wrapper">
+          <div v-show="ActiveBtn" class="wrapper">
             <div class="center">
               <div class="box__top">
                 <div class="logo">
@@ -63,7 +63,7 @@
                   </a>
                 </div>
               </div>
-              <nav v-show="ActiveBtn" class="menu">
+              <nav class="menu">
                 <ul class="menu__wrap menu__alignment flex">
                   <li class="menu__item">
                     <a href="" class="link__fade menu__link">ABOUT</a>
