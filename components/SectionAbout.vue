48ff5d33377c332c716851531a8201c72ced1163
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-04 13:09:34 +0900

    sectionAboutを追加

---
+++ components/SectionAbout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="section">
+    <div class="center">
+      <div class="section__image">
+        <div class="image">
+          <div class="image__profile">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="section__title">
+        <div class="title__wrap">
+          <h2>
+            <span class="title__item">A</span>
+            <span class="title__item">B</span>
+            <span class="title__item">O</span>
+            <span class="title__item">U</span>
+            <span class="title__item">T</span>
+          </h2>
+        </div>
+      </div>
+      <div class="section__text"></div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="container"></div>
+  <div class="main">
+    <div class="section"></div>
+    <SectionAbout />
+    <div class="section">SERVICE</div>
+    <div class="section">BLOG</div>
+    <div class="section">RECIPE</div>
+    <div class="section">CONTACT</div>
+  </div>
 </template>
 
 <script>
+import SectionAbout from '@/components/SectionAbout'
+
 export default {
+  components: {
+    SectionAbout,
+  },
   layout: 'default',
 }
 </script>
