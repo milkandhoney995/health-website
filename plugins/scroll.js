9b9e2c9fdee62a5793da8af7828f45d0d7bab27a
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-07 16:25:57 +0900

    plugins/scroll.jsの中にscroll directiveを作成

--- plugins/scroll.js
+++ plugins/scroll.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 
-// debug
-Vue.directive('bg', {
-  bind(el, binding, vnode) {
-    el.style.backgroundColor = 'lightgreen'
+// Vue.directive('bg', {
+//   bind(el, binding, vnode) {
+//     el.style.backgroundColor = 'lightgreen'
+//   },
+// })
+Vue.directive('scroll', {
+  inserted(element, binding) {
+    const f = function (event) {
+      if (binding.value(event, element)) {
+        window.removeEventListener('scroll', f)
+      }
+    }
+    window.addEventListener('scroll', f)
   },
 })
