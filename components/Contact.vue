2a1dfc10458dbea56e559bc5fcf4988f1e52a437
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-07 09:49:44 +0900

    form関連のvue/scssを追加

---
+++ components/Contact.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="section">
+    <div class="wrapper__explanation">
+      <form action="multipart/form-data" method="post">
+        <div class="form__wrap">
+          <dl class="form__table">
+            <dt class="form__title">COMPANY</dt>
+            <dd class="form__data">
+              <input id="" class="form__input" type="text" name="" />
+            </dd>
+            <dt class="form__title">NAME</dt>
+            <dd class="form__data">
+              <input id="" class="form__input" type="text" name="" />
+            </dd>
+            <dt class="form__title">E-mail</dt>
+            <dd class="form__data">
+              <input id="" class="form__input" type="text" name="" />
+            </dd>
+            <dt class="form__title">TEL</dt>
+            <dd class="form__data">
+              <input id="" class="form__input" type="text" name="" />
+            </dd>
+            <dt class="form__title">MESSAGE</dt>
+            <dd class="form__data">
+              <textarea
+                id=""
+                class="form__textarea border__bottom"
+                type="text"
+                name=""
+                placeholder="( 50文字以上 / more than 50 capital )"
+              />
+              <div class="text__small">
+                お返事をさせていただくまでに、お時間を要する場合がございます。
+                <br />
+                また、内容により、お返事が出来ない場合がございますのでご了承ください。
+              </div>
+            </dd>
+            <dd class="form__btn">
+              <button class="btn-send" type="submit">SEND</button>
+            </dd>
+          </dl>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Contact',
+}
+</script>
