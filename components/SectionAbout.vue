89c544a5447b09f0e5824f9e87b25e68372d5d2f
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-07 17:15:05 +0900

    RGBからHSLに変換する関数

--- components/SectionAbout.vue
+++ components/SectionAbout.vue
@@ -42,12 +42,43 @@ export default {
   methods: {
     handleScroll(event, element) {
       if (window.scrollY > 50) {
-        element.setAttribute(
-          'style',
-          'transition: opacity 0.3s cubic-bezier(0.165, 0.84, 0.44, 1);'
-        )
+        element.setAttribute('style', '.cubic-bezier__all')
       }
       return window.scrollY > 100
+    },
+    // switch:https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Statements/switch
+    // RGBをHSLに変換する方法:https://lab.syncer.jp/Web/JavaScript/Snippet/68/
+    // RGBからHSLに変換する関数
+    changeRgbToHsl(rgb) {
+      const r = rgb[0] / 255
+      const g = rgb[1] / 255
+      const b = rgb[2] / 255
+
+      const min = Math.min(r, g, b)
+      const max = Math.max(r, g, b)
+      const diff = max - min
+      let h = 0
+      const l = (max + min) / 2
+      const s = diff / (1 - Math.abs(max + min - 1))
+      switch (min) {
+        case max:
+          h = 0
+          break
+
+        case r:
+          h = 60 * ((b - g) / diff) + 180
+          break
+
+        case g:
+          h = 60 * ((r - b) / diff) + 300
+          break
+
+        case b:
+          h = 60 * ((g - r) / diff) + 60
+          break
+      }
+
+      return [h, s, l]
     },
   },
 }
