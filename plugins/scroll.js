250ea2be29313a7e733e2cff774eef2c4d0856ae
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-07 17:15:20 +0900

    参考資料追加

--- plugins/scroll.js
+++ plugins/scroll.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 
-// Vue.directive('bg', {
-//   bind(el, binding, vnode) {
-//     el.style.backgroundColor = 'lightgreen'
-//   },
-// })
+// 参照：https://lab.syncer.jp/Web/API_Interface/Reference/IDL/Window/scroll/
+// https://developer.mozilla.org/ja/docs/Web/API/Window/scrollY
+// https://fetchkun.com/entry/nuxt-custom/
+// https://jp.vuejs.org/v2/cookbook/creating-custom-scroll-directives.html
+// https://digipress.info/tech/change-background-color-by-scrolling/#i
 Vue.directive('scroll', {
   inserted(element, binding) {
     const f = function (event) {
