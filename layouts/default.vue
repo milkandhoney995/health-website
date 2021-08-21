87e6cc8e431df02bc271100b4acf1dbd898895b2
harada.utano <harada.utano@cri-mw.co.jp>
2021-08-21 20:51:27 +0900

    スクロール量の変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,9 +23,9 @@ export default {
         document.body.style.backgroundColor = '#fcfbed'
       } else if (scroll > 3000) {
         document.body.style.backgroundColor = '#f0edfc'
-      } else if (scroll > 2500) {
+      } else if (scroll > 2000) {
         document.body.style.backgroundColor = '#bae3cf'
-      } else if (scroll > 1800) {
+      } else if (scroll > 1500) {
         document.body.style.backgroundColor = '#fae8ca'
       } else if (scroll > 900) {
         document.body.style.backgroundColor = '#d6efff'
