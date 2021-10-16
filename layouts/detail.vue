9fecd21f8239085cff3eff2fad91a9b8c840c550
harada.utano <harada.utano@cri-mw.co.jp>
2021-10-17 07:43:06 +0900

    update: layout/blog.vueをdetail.vueに変更

--- layouts/detail.vue
+++ layouts/detail.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <Header />
-    <h1>RECIPE</h1>
     <Nuxt />
+    <Footer />
   </div>
 </template>
 <script>
 import Header from '@/components/Header'
+import Footer from '@/components/Footer'
 
 export default {
   components: {
     Header,
+    Footer,
   },
 }
 </script>
